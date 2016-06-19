e92bdc5be7b0799a98e7f3f42f94e660f556bfcc
tchonny <jon.handam@gmail.com>
2016-06-19 14:55:06 -0300

    base + criação de cilindro

---
+++ RayTrace/projeto/src/cylinder.cpp
@@ -0,0 +1,6 @@
+#include "cylinder.h"
+
+cylinder::cylinder()
+{
+
+}
---
+++ RayTrace/projeto/src/cylinder.h
@@ -0,0 +1,11 @@
+#ifndef CYLINDER_H
+#define CYLINDER_H
+
+
+class cylinder
+{
+public:
+    cylinder();
+};
+
+#endif // CYLINDER_H
